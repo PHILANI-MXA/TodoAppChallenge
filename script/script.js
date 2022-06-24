15a4737c43164cd3b01dc95a587f248f52c7c2e8
PHILANI-MXA <3911755@myuwc.ac.za>
2022-06-24 13:42:21 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,46 @@
+/*let lists = JSON.parse(localStorage.getItem('records')) ?
+    JSON.parse(localStorage.getItem('records')) :
+
+    [{
+        id: 1,
+        item: 'TV Stand',
+        createdDate: new Date()
+    }];
+
+
+function addData(name, item, date)(item) => {
+    {
+        // e.preventDefault();
+        lists.push({
+            id: document.getElementById('[]').value,
+            item: document.getElementById('[]').value,
+            createdDate: document.getElementsByName('[]').Date,
+            return: function addData()
+            co
+        })
+    };
+    // Save data to a localstorage
+    localStorage.setItem('records', JSON.stringify(lists));
+}
+document.querySelector('#addRecord').addEventListener('click', addData);
+// Displaying data
+(function loadData() {
+    console.table(lists);
+})();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function priceSort() {
+    let direction = document.querySelector('#priceSort')
+    let sortedProducts = produt.sort((a, b) => a.price - b.price);
+}*/
